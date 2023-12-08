560a7cc2494697047c244a16a466f751b981d077
Roman Dodin <dodin.roman@gmail.com>
2023-12-08 17:36:56 +0200

    some awful if condition

--- gnmi.go
+++ gnmi.go
@@ -61,7 +61,7 @@ func (a *Agent) getConfigWithGNMI() {
 	a.logger.Debug().Msgf("gNMI Get response: %+v", getResp)
 
 	// log the received config if it is not empty
-	if len(getResp.GetNotification()) != 0 {
+	if len(getResp.GetNotification()) != 0 && len(getResp.GetNotification()[0].GetUpdate()) != 0 {
 		a.logger.Info().Msgf("Config received via gNMI:\n%s", getResp.GetNotification()[0].
 			GetUpdate()[0].
 			GetVal().
